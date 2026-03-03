4ac1ffed3895a1597049a2265dd32e36cfd93324
tiffany <tiffany871205@gmail.com>
2026-03-04 00:35:58 +0800

    feat: admin router

--- src/assets/layout/AdminLayout.jsx
+++ src/assets/layout/AdminLayout.jsx
@@ -1,5 +1,34 @@
+import { Link, Outlet } from "react-router";
+
 function AdminLayout() {
-  return <>我是後台排版頁面</>;
+  return (
+    <>
+      <ul className="nav mt-2 mb-4 ms-2">
+        <li className="nav-item">
+          <Link className="nav-link nav-item" aria-current="page" to="/admin/adminOrders">
+            訂單
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link nav-item" aria-current="page" to="/admin/adminProducts">
+            商品管理
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link nav-item" aria-current="page" to="/admin/login">
+            登入
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link nav-item" aria-current="page" to="/">
+            返回前台
+          </Link>
+        </li>
+      </ul>
+      我是後台排版頁面
+      <Outlet />
+    </>
+  );
 }
 
 export default AdminLayout;
